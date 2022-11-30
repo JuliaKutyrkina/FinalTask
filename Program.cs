9c8e6e221fc85b44ecc22bd0dfecf45c83b9099c
JuliaKutyrkina <u_kutyrkina@mail.ru>
2022-11-30 09:26:12 +0300

    Создание программного файла

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿//итоговое задание
+void InputArray(string [] array)
+{ Console.Write("Начальная строка: ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if(i < array.Length - 1)
+        Console.Write(", ");
+    }
+    Console.WriteLine();
+}
+
+
+void ReleaseArray(string [] array)
+{  Console.Write("Конечная строка: ");
+    for(int i = 0; i < array.Length; i++)
+    {   int c = array[i].Length;
+        int j = 0;
+        
+        if (c < 4)
+        {
+            j = i;
+            Console.Write($"{array[j]}");
+            if(i < array.Length - 1)
+            Console.Write(", ");
+            
+        }
+        else Console.Write("");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите массив данных: ");
+string[] array = Console.ReadLine().Split(' ');
+InputArray(array);
+ReleaseArray(array);
